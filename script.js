83f667e7be7ce9c12d529fc5b9335fa388d03705
Sanket-shiwakoti2 <125861842+Sanket-shiwakoti2@users.noreply.github.com>
2023-02-20 13:35:48 +0545

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    var navList = document.getElementById("nav-list");
+    var searchbox = document.getElementsByClassName("sb")
+    navList.classList.toggle("active");
+  }
+  
